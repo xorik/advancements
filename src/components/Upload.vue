974bf4d8c5eeb38bb3ec47edbcc60a3503a92d8c
xorik <xorik@users.noreply.github.com>
2022-11-12 11:24:39 +0100

    More clear upload instructions

--- src/components/Upload.vue
+++ src/components/Upload.vue
@@ -12,15 +12,16 @@ const { isOverDropZone } = useUpload(emit, dropZoneRef)
 <template>
   <div
     ref="dropZoneRef"
-    class="flex items-center items-center justify-around rounded-xl border-2 border-dashed p-10"
+    class="flex items-center items-center justify-around rounded-xl border-2 border-dashed p-8"
     :class="{ 'bg-base-300': isOverDropZone }"
   >
     <i-ph-upload-simple-fill class="flex-shrink-0 text-5xl" />
     <div class="hidden flex-shrink sm:inline-block">
       <ul class="list-disc">
-        <li>Select your world</li>
-        <li>Click "Edit" then "Open World Folder"</li>
-        <li>Open "advancements" folder and drop a JSON file</li>
+        <li>In Minecraft select your world and click "Edit"</li>
+        <li>Then click "Open World Folder"</li>
+        <li>Open "advancements" folder</li>
+        <li>Drag & drop a JSON file in this box</li>
       </ul>
     </div>
   </div>
